6dcc74189510997a8755fd302311dfa6271c6657
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2021-06-15 13:21:38 +0200

    first iteration of converter

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,51 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    const idx = input.match(/[^\d]+/i)["index"];
+    if (idx === 0) return 1;
+    return input.substr(0, idx);
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (input) {
+    const idx = input.match(/[^\d]+/i)["index"];
+    return input.substr(idx);
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    return {
+      gal: "l",
+      lbs: "kg",
+      mi: "km",
+    }[initUnit];
+  };
+
+  this.spellOutUnit = function (unit) {
+    return {
+      gal: "gallon",
+      lbs: "pounds",
+      mi: "miles",
+      l: "litre",
+      kg: "kilogram",
+      km: "kilometer",
+    }[unit];
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    switch (initUnit) {
+      case "gal":
+        return initNum * galToL;
+      case "lbs":
+        return initNum * lbsToKg;
+      case "mi":
+        return initNum * miToKm;
+    }
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
   };
-  
 }
 
 module.exports = ConvertHandler;
